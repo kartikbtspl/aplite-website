1dad07e3afc64c8216bbbc6dee61cda5d098a805
AvantikaBrilliantech <avantika@brilliantechsoft.com>
2025-11-12 17:43:15 +0530

    Animation changes, Added responsiveness

--- app/solutions/ap-teams/Sections/Section3.tsx
+++ app/solutions/ap-teams/Sections/Section3.tsx
@@ -1,11 +1,30 @@
 import BlankCard from "@/components/ui/BlankCard";
 import Button from "@/components/ui/Buttons";
 import { CircleArrowOutUpRight } from "lucide-react";
+
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+
 export default function Section3() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, margin: "-50px" });
+
+  const fadeInAnimation = {
+    initial: { opacity: 0, y: 30 },
+    animate: isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 30 },
+    transition: { duration: 0.8 },
+  };
+
   return (
-    <BlankCard>
-      <div className="p-6 space-y-6">
-        <div className="flex justify-center items-start">
+    <BlankCard ref={ref}>
+      <motion.div
+        key="hero-sec"
+        initial={fadeInAnimation.initial}
+        animate={fadeInAnimation.animate}
+        transition={fadeInAnimation.transition}
+        className="p-6 space-y-6"
+      >
+        <div className="text-center">
           <p className="text-2xl font-medium  leading-tight">
             Ready to transform your AP process?
           </p>
@@ -26,7 +45,7 @@ export default function Section3() {
             size="sm"
           />
         </div>
-      </div>
+      </motion.div>
     </BlankCard>
   );
 }
